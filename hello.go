14513058be2db40ff3a474aa2f72667465843f38
Devair Luiz Moitim <dmoitim@gmail.com>
2020-10-11 12:18:22 -0300

    Criando funções e implementação do sair

--- hello.go
+++ hello.go
@@ -1,20 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "DevA"
-	versao := 1.1
+	exibeIntroducao()
+	exibeMenu()
 
-	fmt.Println("Olá, ", nome)
-	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
-
-	var comando int
-	fmt.Scan(&comando)
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -22,8 +17,31 @@ func main() {
 	case 2:
 		fmt.Println("Exibindo logs...")
 	case 0:
-		fmt.Println("Saindo do programa...")
+		fmt.Println("Saindo do programa.")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando.")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "DevA"
+	versao := 1.1
+
+	fmt.Println("Olá, ", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+
+	return comandoLido
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
 }
